fdc3745b763787fb377d018a3b48eddb3ecfb583
Rayk <raykrocha1@gmail.com>
2020-06-17 15:06:49 -0300

    modified delete button on pressed

--- the_good_bot_app/lib/views/home_page.dart
+++ the_good_bot_app/lib/views/home_page.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:the_good_bot_app/widgets/confirm_delete.dart';
+import '../repository/bot_repository.dart';
 
+// ignore: must_be_immutable
 class HomePage extends StatelessWidget {
+  BotRepository botRepository = new BotRepository();
+  Confirm confirm = new Confirm();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromRGBO(238, 15, 82, .9),
-          title: Text("The Good Bot"),
-          <Widget>[
-                 IconButton(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(238, 15, 82, .9),
+        title: Text("The Good Bot"),
+        actions: <Widget>[
+          IconButton(
             icon: Icon(Icons.delete_sweep),
             onPressed: () {
-              botRepository.deleteAll();
+              confirm.deleteDialog(context);
             },
           ),
-         ],
-        ),
+        ],
+      ),
       body: Container(
         padding: EdgeInsets.only(top: 60, left: 40, right: 40),
         color: Color.fromRGBO(247, 247, 247, .9),
@@ -37,8 +42,8 @@ class HomePage extends StatelessWidget {
                   "/chat_page",
                 );
               },
-
-              child: const Text('Abrir o ChatBot', style: TextStyle(fontSize: 20)),
+              child:
+                  const Text('Abrir o ChatBot', style: TextStyle(fontSize: 20)),
             ),
           ],
         ),
