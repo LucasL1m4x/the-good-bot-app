0a6846f074d64213bc4ae098efd23bcd16a95381
Lucas Lima <lucasl1m4x@gmail.com>
2020-06-16 22:47:43 -0300

    Atualizando alguns erros de codigo
    Atualizando alguns erros de codigo

---
+++ chat_message_list_item.dart
@@ -0,0 +1,33 @@
+import 'package:the_good_bot_app/models/chat_message.dart';
+import 'package:flutter/material.dart';
+
+class ChatMessageListItem extends StatelessWidget {
+  final ChatMessage chatMessage;
+
+  ChatMessageListItem({this.chatMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    return chatMessage.type == 'sent'
+        ? _showSentMessage()
+        : _showReceivedMessage();
+  }
+
+  Widget _showSentMessage({EdgeInsets padding, TextAlign textAlign}) {
+    return ListTile(
+      contentPadding: EdgeInsets.fromLTRB(64.0, 0.0, 8.0, 0.0),
+      trailing: CircleAvatar(child: Text(chatMessage.name.toUpperCase()[0])),
+      title: Text(chatMessage.name, textAlign: TextAlign.right),
+      subtitle: Text(chatMessage.text, textAlign: TextAlign.right),
+    );
+  }
+
+  Widget _showReceivedMessage() {
+    return ListTile(
+      contentPadding: EdgeInsets.fromLTRB(8.0, 0.0, 64.0, 0.0),
+      leading: CircleAvatar(child: new Image.asset("assets/robo.png")),
+      title: Text(chatMessage.name, textAlign: TextAlign.left),
+      subtitle: Text(chatMessage.text, textAlign: TextAlign.left),
+    );
+  }
+}
